af3bf0353d3d40baf397d26a90e8c2b33a9d652b
celsorodrigo <celsorodrigoma@gmail.com>
2022-10-04 15:34:47 -0300

    Concluído 4° requisito.

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -12,11 +12,26 @@ const generateToken = () => {
   return token;
 };
 
-router.post('/', (_req, res) => {
+const validateEmail = (email) => !email.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/);
+
+const validateSize = (string) => !string || !string.length;
+
+const validateForm = (email, password) => {
+  if (validateSize(password)) throw new Error('O campo "password" é obrigatório');
+  if (password.length < 6) throw new Error('O "password" deve ter pelo menos 6 caracteres');
+  if (validateSize(email)) throw new Error('O campo "email" é obrigatório');
+  if (validateEmail(email)) {
+    throw new Error('O "email" deve ter o formato "email@email.com"');
+  }
+};
+
+router.post('/', (req, res) => {
   try {
+    const { email, password } = req.body;
+    validateForm(email, password);
     res.status(200).json({ token: generateToken() });
   } catch (err) {
-    res.status(500).json({ message: err });
+    res.status(400).json({ message: err.message });
   }
 });
 
