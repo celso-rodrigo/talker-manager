d32a9b0eefe449122c6b6d89ea0d0f9b2f6b5537
celsorodrigo <celsorodrigoma@gmail.com>
2022-10-04 14:58:29 -0300

    Concluído 2° requisito.

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -6,16 +6,24 @@ const fs = require('fs').promises;
 router.get('/', async (_req, res) => {
   try {
     const data = await fs.readFile('./src/talker.json', 'utf-8');
-    const json = await JSON.parse(data);
+    const json = JSON.parse(data);
     res.status(200).json(json);
   } catch (err) {
     res.status(500).json({ message: err });
   }
 });
 
-// router.get('/:id', (req, res) => {
-//   const { id } = req.params;
-// });
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await fs.readFile('./src/talker.json', 'utf-8');
+    const personById = JSON.parse(data).filter((person) => person.id === Number(id));
+    if (!personById.length) throw new Error('Pessoa palestrante não encontrada');
+    res.status(200).json(...personById);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+});
 
 // router.get('/search', (req, res) => {});
 
