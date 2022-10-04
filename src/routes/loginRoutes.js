70b88c179bff4248631db809e57b5ffaa2553729
celsorodrigo <celsorodrigoma@gmail.com>
2022-10-04 15:12:30 -0300

    Concluído 3° requisito.

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -1,6 +1,23 @@
-// const express = require('express');
-// const router = express.Router();
+const express = require('express');
 
-// router.post('/', (req, res) => {});
+const router = express.Router();
 
-// module.exports = router;
+const generateToken = () => {
+  const characters = '1234567890qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM';
+  let token = '';
+  for (let index = 0; index < 16; index += 1) {
+    const random = Math.floor(Math.random() * ((characters.length - 1) - 0));
+    token = token.concat('', characters[random]);
+  }
+  return token;
+};
+
+router.post('/', (_req, res) => {
+  try {
+    res.status(200).json({ token: generateToken() });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
+
+module.exports = router;
